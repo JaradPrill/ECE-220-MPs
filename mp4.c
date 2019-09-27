4fa3bd2809154a451d01551ad16e9154f10a532a
Jarad Prill <jaradjp2@illinois.edu>
2019-09-27 14:34:11 -0500

    Co-authored-by: Jiang, Bill <bozhaoj2@illinois.edu>

--- mp4.c
+++ mp4.c
@@ -2,28 +2,55 @@
 #include <stdlib.h>
 
 int is_prime(int num);
+int print_semiprimes(int a, int b);
 
 int main(){
-    int x, y;
+    int a, b;
     printf("Input two numbers: ");
-    scanf("%d %d",&x,&y);
+    scanf("%d %d", &a, &b);
     printf("%d %d\n",x,y);
-    //print_semiprimes(x,y);
+    if( a <= 0 || b <= 0 ){
+        printf("Inputs should be positive integers\n");
+        return 1;
+    }
+    
+    if( a > b ){
+        printf("The first number should be smaller than or equal to the second number\n");
+        return 1;
+    }
+
+    // TODO: call the print_semiprimes function to print semiprimes in [a,b] (including a and b)
+    print_semiprimes(x,y);
     printf("%d\n",is_prime(x)); //debugg test for is_prime
     return 0;
 }
 
-// int print_semiprimes(int a, int b){
-//     for (int n=a; n<=b; n++)
-//     {
-//         for (int k = 2; n <= n-1; k++)
-//         {
+/*
+ * TODO: implement this function to print all semiprimes in [a,b] (including a, b).
+ * Input   : a, b (a should be smaller than or equal to b)
+ * Return  : 0 if there is no semiprime in [a,b], else 1
+ */
+ int print_semiprimes(int a, int b){
+     for (int i=a; i<=b; i++)
+     {
+         for (int j = 2; j <= n-1; j++)
+         {
+            int j_prime = is_prime(j); //check if j is a prime number
+            int ij_prime = 0; //
+            if (i % j==0) //check if j is a factor of i
+            {
+                int ij = (i/j)
+                int ij_prime = is_prime(ij);
+            }
             
+            if (j_prime==1) && (ij_prime==1) //checks if i and j and i/j are primes
+            {
+                printf("%d", i); //print i if both j and i/j are primes
+            }
+         }
 
-//         }
-
-//     }
-// }
+     }
+ }
 
 
 /*
