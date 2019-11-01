c8ca7cad71f629da422171bcc46365356da73eae
jiangbill123 <55008400+jiangbill123@users.noreply.github.com>
2019-11-01 15:06:58 -0500

    update maze.c

--- mp9/maze.c
+++ mp9/maze.c
@@ -12,64 +12,53 @@
  */
 maze_t * createMaze(char * fileName)
 {
+    // Your code here. Make sure to replace following line with your own code.
     //Create file pointer to fileName given
     FILE *file;
 
     //Open the text file containing maze structure
     file = fopen(fileName, "r");
-    if(file == NULL) printf("did not find file\n");
 
     //Create maze pointer and allocate memory
     maze_t * maze = malloc(sizeof(maze_t));
-    printf("successfully allocated maze structure\n");
-
+    // assign width and hight from file to maze struct
     int m, n;
+    char c;
     fscanf(file, "%d %d", &m, &n);
-    printf("Width: %d Height: %d\n", n,m);
-
     maze->width = n;
-    maze->height = m;
-    printf("assigned width and height %d\n", maze->height);
+    maze->height = m;   
 
     //Allocate memory for cells row pointer 
     maze->cells = malloc(sizeof(maze->cells)*(maze->height)); 
-    printf("allocated cell pointer array memory\n");
-    
     
     //Allocate memory for the size of a row
     int i,j;
     for(i = 0; i < maze->height; i++){
         maze->cells[i] = malloc(sizeof(maze->cells[i]) * maze->width);
-        if(maze->cells[i] == NULL) printf("Didn't assign memory\n");
+        // if(maze->cells[i] == NULL) printf("Didn't assign memory\n");
     }
-    printf("allocated memory in each char arrary\n");
+    // printf("allocated memory in each char arrary\n");
 
-    //Discard unneeded newline
-    fscanf(file, "%c", &m);
-    
+    fscanf(file, "%c", &c);//Discard unneeded newline char
 
     //Populate each element of cells
     for(i = 0; i < maze->height; i++){
         for(j = 0; j < maze->width; j++){
-            fscanf(file, "%c", &m);
-            printf("Scanned: %c at lcoation [%d][%d]\n", m, i, j);
-            maze->cells[i][j] = m;
-            printf("stored char in [%d][%d]\n", i, j);
-            
-            if(maze->cells[i][j] == 'S'){
+            fscanf(file, "%c", &c);
+            //printf("Scanned: %c at lcoation [%d][%d]\n", m, i, j);
+            maze->cells[i][j] = c;
+            //printf("stored char in [%d][%d]\n", i, j);
+            if(maze->cells[i][j] == 'S'){//assign variables to struct
                 maze->startRow = i;
                 maze->startColumn = j;
             }
-            if(maze->cells[i][j] == 'E'){
+            if(maze->cells[i][j] == 'E'){//assign variables to struct
                 maze->endRow = i;
                 maze->endColumn = j;
             }
-            
         }
-        fscanf(file, "%c", &m);    
-
+        fscanf(file, "%c", &c);//discard newline character after each row    
     }
-    
 
     return maze;
 }
@@ -97,7 +86,20 @@ void destroyMaze(maze_t * maze)
  */
 void printMaze(maze_t * maze)
 {
-    // Your code here.
+    // Your code here
+    int row = maze->height;
+    int col = maze->width;
+    int i,j;
+    for (i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            printf("%c",maze->cells[i][j]);
+        }
+        if (i<row-1) //do not print new line for last row
+            printf("\n"); 
+    }
+
 }
 
 /*
