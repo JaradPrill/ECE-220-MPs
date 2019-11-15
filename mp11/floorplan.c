2aca9a5cfa9ea20d948dfeeb85a1c8c9c4d17e30
jiangbill123 <55008400+jiangbill123@users.noreply.github.com>
2019-11-15 15:30:19 -0600

    update all function except for init

--- mp11/floorplan.c
+++ mp11/floorplan.c
@@ -135,27 +135,15 @@ void swap_topology(node_t* a, node_t* b) {
   assert(a->parent != NULL && b->parent != NULL);
  
   // TODO:
-  node_t* temp = a;
+  node_t* temp = a->parent;
+  a->parent = b->parent;
+  b->parent = temp;
 
-  // change parent of b to parent of a, and change left/right children of a to point to b
-  if (a->parent->left == a) { // a is the left child
-    a->parent->left = b;
-    b->parent = a->parent;
-  } 
-  else if (a->parent->right == a) { // a is the right child
-    a->parent->right = b;
-    b->parent = a->parent;
-  }
+  if (b->parent->left == b) b->parent->left = a;
+  else b->parent->right = a;
 
-  // change parent of a to parent of b, and change left/right children of b to point to a
-  if (b->parent->left == b) { // b is the left child
-    b->parent->left = temp;
-    temp->parent = b->parent; //temp is a
-  } 
-  else if (b->parent->right == b) { // b is the right child
-    b->parent->right = temp;
-    temp->parent = b->parent;
-  }
+  if (a->parent->left == a) a->parent->left = b;
+  else a->parent->right = b;
   
   return;
 }
