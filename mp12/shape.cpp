9dbda5627daa4408296c8d1ee716b557533d65b4
Jarad Prill <jaradjp2@illinois.edu>
2019-12-06 14:05:44 -0600

    mild progress on mp12, disregard this commit

--- mp12/shape.cpp
+++ mp12/shape.cpp
@@ -15,8 +15,8 @@
 //Please implement the member functions of Rectangle:
 //constructor, getArea(), getVolume(), operator+, operator-
 //@@Insert your code here
-
-
+double Rectangle::getArea()const{return width_*length_;}
+double Rectangle::getVolume()const{return 0;}
 
 double Rectangle::getWidth()const{return width_;}
 double Rectangle::getLength()const{return length_;}
@@ -54,7 +54,40 @@ double RectPrism::getLength()const{return length_;}
 // Return a vector of pointers that points to the objects 
 vector<Shape*> CreateShapes(char* file_name){
 	//@@Insert your code here
-	return vector<Shape*>(0, NULL); // please remeber to modify this line to return the correct value
+	int num_of_shapes, i;
+	vector<Shape*> vector_array;
+	string shape_name;
+	ifstream ifs ("test.txt", std::ifstream::in);
+	ifs >> num_of_shapes;
+	for (i=0;i<num_of_shapes;i++){
+		ifs >> shape_name;
+		if (shape_name=="circle"){
+			int radius;
+			ifs >> radius;
+			Shape* shape_ptr = new Circle(radius);
+			vector_array.push_back(shape_ptr);
+		}
+		else if (shape_name == "rectangle"){
+			int width, length;
+			ifs >> width >> length;
+			Shape* shape_ptr = new Rectangle(width, length);
+			vector_array.push_back(shape_ptr);
+		}
+		else if (shape_name == "RectPrism"){
+			int width, height, length;
+			ifs >> width >> height >> length;
+			Shape* shape_ptr = new RectPrism(width, height, length);
+			vector_array.push_back(shape_ptr);
+		}
+		else{
+			int radius;
+			ifs >> radius;
+			Shape* shape_ptr = new Sphere(radius);
+			vector_array.push_back(shape_ptr);
+		}
+	}
+	ifs.close();
+	return vector_array; // please remeber to modify this line to return the correct value
 }
 
 // call getArea() of each object 
